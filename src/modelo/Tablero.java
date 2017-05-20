070c549086a446f0d907134229e89d7869b98123
aledark <konachan101@gmail.com>
2017-05-20 13:04:19 -0500

    Mejora del doble for
    Adios constructor por defecto

--- src/modelo/Tablero.java
+++ src/modelo/Tablero.java
@@ -10,14 +10,12 @@ public class Tablero{
   boolean turno;
   ArraList<Ficha> bolsaFichas;
 
-  public Tablero(){
-    gameOver = false;
-    turno = false;
-    bolsaFichas = new ArrayList<Ficha>();
-  }
   public Tablero(Jugador j1, Jugador j2){
     this.j1 = j1;
     this.j2 = j2;
+    gameOver = false;
+    turno = false;
+    bolsaFichas = new ArrayList<Ficha>();
     tablero = new ArrayList<ArrayList<Ficha>>();
 
     for (int i = 0; i < 11; i++) {
@@ -27,29 +25,22 @@ public class Tablero{
     for (int i=0;i<11;i++ ) {
       if(i<6){
         for (int j=0;j<i+6;j++) {
-          if(i==0 && j==0){
-            tablero.get(i).add(new Ficha(i,j,null,1));
-          }else if(i==0 && j==5){
-            tablero.get(i).add(new Ficha(i,j,null,2));
-          }else if(i==5 && j==0){
-            tablero.get(i).add(new Ficha(i,j,null,3));
-          }else if(i==5 && j==10){
-            tablero.get(i).add(new Ficha(i,j,null,4));
-          }else{
-            tablero.get(i).add(new Ficha(i,j,null,0));
-          }
+          tablero.get(i).add(new Ficha(i,j,null,0));
         }
       }else{
         for (int j=0;j<16-i;j++) {
-          if(i==10 && j==0){
-            tablero.get(i).add(new Ficha(i,j,null,5));
-          }else if(i==10 && j==5){
-            tablero.get(i).add(new Ficha(i,j,null,6));
-          }else{
-            tablero.get(i).add(new Ficha(i,j,null,0));
-          }
+          tablero.get(i).add(new Ficha(i,j,null,0));
+
         }
       }
     }
+
+    tablero.get(0).get(0).setColor(1);
+    tablero.get(0).get(5).setColor(2);
+    tablero.get(5).get(0).setColor(3);
+    tablero.get(5).get(10).setColor(4);
+    tablero.get(10).get(0).setColor(5);
+    tablero.get(10).get(5).setColor(6);
+
   }
 }
